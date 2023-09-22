1eba19eb572db5985b206695d18df28fe6c4d11c
Jermiah <jermiahjoseph@gmail.com>
2023-09-22 16:45:39 +0000

    add folderupdate

--- GCS_make_objects_public.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Read the file line by line
-while IFS= read -r path; do
-  # Run the gcloud command for each path
-  echo " gcloud storage objects update '$path' --add-acl-grant=entity=AllUsers,role=READER"
-  gcloud storage objects update "$path" --add-acl-grant=entity=AllUsers,role=READER
-done < "CCLE_data_paths.txt"
